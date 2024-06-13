1ed27564f0f3dc5baad98ec092dee37764162e4e
Mukhtarrahimi <mukhtarrahimi110@gmail.com>
2024-06-13 14:21:47 +0430

    header code and style

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ImageGenerator } from "./components/ImageGenerator";
 
 const App = () => {
-  return <div>
+  return <div className="w-full flex items-center justify-center p-10 min-h-screen bg-gradient-to-tr from-slate-900 to-slate-700">
     <ImageGenerator/>
   </div>;
 };
--- src/components/ImageGenerator.jsx
+++ src/components/ImageGenerator.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import default_img from '../assets/person-playing-3d-video-games-device.jpg'
 
 export const ImageGenerator = () => {
   return (
-    <div>ImageGenerator</div>
+    <div className='flex flex-col items-center justify-center'>
+      <h1 className='font-Poetsen text-2xl md:text-3xl lg:text-4xl text-white'>AI Image <span className='text-yellow-500'>Generator</span></h1>
+      <div className="img w-72 rounded-md overflow-hidden mt-8 mb-4">
+        <img className='w-full object-cover' src={default_img} alt="" />
+      </div>
+    </div>
   )
 }
